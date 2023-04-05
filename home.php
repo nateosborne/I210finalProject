e68503dea6ebd64d06a261c133286557e14c5ae6
nateosborne <natedresdeno@gmail.com>
2023-04-05 16:01:57 -0400

    Nav bar

--- home.php
+++ home.php
@@ -1,1 +1,29 @@
 <?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Final Project</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://kit.fontawesome.com/d2db57536d.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="nav">
+    <div class="logo"></div>
+    <div class="links">
+        <a href="">Home</a>
+        <a href="">Packs</a>
+        <a href="">Contact</a>
+    </div>
+    <div class="icons">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <i class="fa-solid fa-circle-user"></i>
+        <i class="fa-solid fa-bag-shopping"></i>
+    </div>
+</div>
+
+</body>
+</html>
